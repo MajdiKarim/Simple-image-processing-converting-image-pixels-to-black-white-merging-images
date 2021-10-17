afa3a4b2a323ac4266c5893b0521ebd9029a6080
MajdiKarim <majdii.karim@gmail.com>
2021-10-17 20:48:56 +0100

    update convert class

--- convert.java
+++ convert.java
@@ -12,7 +12,7 @@ public class convert {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in); 
 		System.out.print("Enter the corresponding path to your input image:\n");  
-		String str= sc.nextLine();              //reads string
+		String str= sc.nextLine();              
 		BufferedImage bi = null;
 		System.out.print("Enter the threshold from which the pixel value will change:\n");
 		Scanner threshold =new Scanner(System.in); 
@@ -25,17 +25,15 @@ public class convert {
 		catch (IOException e) {
 			
 		}
-		//System.out.println(bi);
-		//BufferedImage ba;
-		 // Modification de la couleur du pixel
+		
 	     
 		
 		
-		int w = bi.getWidth(); // la largeur de l'image
-		int h = bi.getHeight(); // Sa hauteur
+		int w = bi.getWidth(); 
+		int h = bi.getHeight(); 
 		for(int i=0; i<w; i++){
 		for(int j=0; j<h; j++){
-		int rgb = bi.getRGB(i,j); /* Accéder au pixel (i, j) */
+		int rgb = bi.getRGB(i,j); 
 		
 		if (rgb > s) {
 		bi.setRGB(i, j, 255);
@@ -49,7 +47,7 @@ public class convert {
 	}
 		System.out.print("Enter the corresponding path where to the processed image: will be stored\n");  
 		String processed = sc.nextLine();     
-		try { // l'image lu sera sauvegarder dans un autre fichier
+		try { 
 			ImageIO.write(bi, "JPG", new File(processed));
 			}
 			catch (IOException e) { e.printStackTrace();}
